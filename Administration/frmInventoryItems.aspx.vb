cea88bf4dc8637dfcf8cc5b81a58a755ed2e5fee
dsamaddar <debumist@gmail.com>
2017-06-29 00:44:00 +0600

    1st Submission

---
+++ Administration/frmInventoryItems.aspx.vb
@@ -0,0 +1,165 @@
+﻿
+Partial Class Administration_frmInventoryItems
+    Inherits System.Web.UI.Page
+
+    Dim UnitTypeData As New clsUnitTypeDataAccess()
+    Dim ItemData As New clsItemDataAccess()
+
+    Private Sub MessageBox(ByVal strMsg As String)
+        Dim lbl As New System.Web.UI.WebControls.Label
+        lbl.Text = "<script language='javascript'>" & Environment.NewLine _
+                   & "window.alert(" & "'" & strMsg & "'" & ")</script>"
+        Page.Controls.Add(lbl)
+    End Sub
+
+    Protected Sub ShowUnitTypeList()
+        drpUnitType.DataTextField = "UnitType"
+        drpUnitType.DataValueField = "UnitTypeID"
+        drpUnitType.DataSource = UnitTypeData.fnGetUnitTypeList()
+        drpUnitType.DataBind()
+    End Sub
+
+    Protected Sub GetInventoryItemDetails()
+        grdAvailableItems.DataSource = ItemData.fnGetItemListDetails()
+        grdAvailableItems.DataBind()
+    End Sub
+
+    Protected Sub btnCancelInputItemType_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnCancelInputItemType.Click
+        ClearItemInput()
+    End Sub
+
+    Protected Sub ClearItemInput()
+        txtItemCode.Text = ""
+        txtItemName.Text = ""
+        txtLowBalanceReport.Text = ""
+        txtMaxRequisition.Text = "10000"
+        drpUnitType.SelectedIndex = -1
+        chkItemIsActive.Checked = False
+
+        grdAvailableItems.SelectedIndex = -1
+
+        btnAddItem.Visible = True
+        btnUpdateInventoryItems.Visible = False
+        hdFldItemID.Value = ""
+    End Sub
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Dim MenuIDs As String
+
+        MenuIDs = Session("PermittedMenus")
+
+        If InStr(MenuIDs, "MngItm~") = 0 Then
+            Response.Redirect("~\frmAILogin.aspx")
+        End If
+
+        If Not IsPostBack Then
+            ShowUnitTypeList()
+            btnUpdateInventoryItems.Visible = False
+            GetInventoryItemDetails()
+        End If
+    End Sub
+
+    Protected Sub btnUpdateInventoryItems_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnUpdateInventoryItems.Click
+
+        Dim Items As New clsItems()
+        Try
+            Items.ItemID = hdFldItemID.Value
+            Items.ItemName = txtItemName.Text
+            Items.ItemCode = txtItemCode.Text
+            Items.LowBalanceReport = txtLowBalanceReport.Text
+            Items.MaxAllowableRequisition = txtMaxRequisition.Text
+            Items.UnitTypeID = drpUnitType.SelectedValue
+            If chkItemIsActive.Checked = True Then
+                Items.IsActive = True
+            Else
+                Items.IsActive = False
+            End If
+
+            Items.EntryBy = Session("LoginUserID")
+
+            Dim Check As Integer = ItemData.fnUpdateInventoryItems(Items)
+
+            If Check = 1 Then
+                MessageBox("Item Updated Successfully.")
+                GetInventoryItemDetails()
+                ClearItemInput()
+            Else
+                MessageBox("Error Found.")
+            End If
+        Catch ex As Exception
+            MessageBox(ex.Message)
+        End Try
+
+    End Sub
+
+    Protected Sub grdAvailableItems_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles grdAvailableItems.SelectedIndexChanged
+
+        grdAvailableItems.Focus()
+        btnAddItem.Visible = False
+        btnUpdateInventoryItems.Visible = True
+
+        Dim lblItemID As New System.Web.UI.WebControls.Label()
+        Dim lblItemName As New System.Web.UI.WebControls.Label()
+        Dim lblItemCode As New System.Web.UI.WebControls.Label()
+        Dim lblUnitTypeID As New System.Web.UI.WebControls.Label()
+        Dim lblLowBalanceReport As New System.Web.UI.WebControls.Label()
+        Dim lblMaxAllowableRequisition As New System.Web.UI.WebControls.Label()
+        Dim lblIsActive As New System.Web.UI.WebControls.Label()
+
+        lblItemID = grdAvailableItems.SelectedRow.FindControl("lblItemID")
+        lblItemName = grdAvailableItems.SelectedRow.FindControl("lblItemName")
+        lblItemCode = grdAvailableItems.SelectedRow.FindControl("lblItemCode")
+        lblUnitTypeID = grdAvailableItems.SelectedRow.FindControl("lblUnitTypeID")
+        lblLowBalanceReport = grdAvailableItems.SelectedRow.FindControl("lblLowBalanceReport")
+        lblMaxAllowableRequisition = grdAvailableItems.SelectedRow.FindControl("lblMaxAllowableRequisition")
+        lblIsActive = grdAvailableItems.SelectedRow.FindControl("lblIsActive")
+
+        hdFldItemID.Value = lblItemID.Text
+        txtItemName.Text = lblItemName.Text
+        txtItemCode.Text = lblItemCode.Text
+        drpUnitType.SelectedValue = lblUnitTypeID.Text
+        txtLowBalanceReport.Text = lblLowBalanceReport.Text
+        txtMaxRequisition.Text = lblMaxAllowableRequisition.Text
+
+        If lblIsActive.Text = "Active" Then
+            chkItemIsActive.Checked = True
+        Else
+            chkItemIsActive.Checked = False
+        End If
+
+    End Sub
+
+    Protected Sub btnAddItem_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnAddItem.Click
+        Dim Items As New clsItems()
+
+        Try
+            Items.ItemName = txtItemName.Text
+            Items.ItemCode = txtItemCode.Text
+            Items.LowBalanceReport = txtLowBalanceReport.Text
+            Items.MaxAllowableRequisition = txtMaxRequisition.Text
+            Items.UnitTypeID = drpUnitType.SelectedValue
+            If chkItemIsActive.Checked = True Then
+                Items.IsActive = True
+            Else
+                Items.IsActive = False
+            End If
+
+            Items.EntryBy = Session("LoginUserID")
+
+            Dim Check As Integer = ItemData.fnInsertInventoryItems(Items)
+
+            If Check = 1 Then
+                MessageBox("Item Inserted Successfully.")
+                GetInventoryItemDetails()
+                ClearItemInput()
+            Else
+                MessageBox("Error Found.")
+            End If
+        Catch ex As Exception
+            MessageBox(ex.Message)
+        End Try
+       
+    End Sub
+
+End Class
---
+++ App_Code/DataAccess/clsItemDataAccess.vb
@@ -0,0 +1,168 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data.SqlClient
+Imports System.Data
+
+Public Class clsItemDataAccess
+
+    Dim con As SqlConnection = New SqlConnection(ConfigurationManager.ConnectionStrings("HelpDeskConnectionString").ConnectionString)
+
+#Region " Insert Inventory Items "
+
+    Public Function fnInsertInventoryItems(ByVal Items As clsItems) As Integer
+        Try
+            Dim cmd As SqlCommand = New SqlCommand("spInsertInventoryItems", con)
+            con.Open()
+            cmd.CommandType = CommandType.StoredProcedure
+            cmd.Parameters.AddWithValue("@ItemName", Items.ItemName)
+            cmd.Parameters.AddWithValue("@ItemCode", Items.ItemCode)
+            cmd.Parameters.AddWithValue("@UnitTypeID", Items.UnitTypeID)
+            cmd.Parameters.AddWithValue("@LowBalanceReport", Items.LowBalanceReport)
+            cmd.Parameters.AddWithValue("@MaxAllowableRequisition", Items.MaxAllowableRequisition)
+            cmd.Parameters.AddWithValue("@IsActive", Items.IsActive)
+            cmd.Parameters.AddWithValue("@EntryBy", Items.EntryBy)
+            cmd.ExecuteNonQuery()
+            con.Close()
+            Return 1
+        Catch ex As Exception
+            If con.State = ConnectionState.Open Then
+                con.Close()
+            End If
+            Return 0
+        End Try
+    End Function
+
+#End Region
+
+#Region " Update Inventory Items "
+
+    Public Function fnUpdateInventoryItems(ByVal Items As clsItems) As Integer
+        Try
+            Dim cmd As SqlCommand = New SqlCommand("spUpdateInventoryItems", con)
+            con.Open()
+            cmd.CommandType = CommandType.StoredProcedure
+            cmd.Parameters.AddWithValue("@ItemID", Items.ItemID)
+            cmd.Parameters.AddWithValue("@ItemName", Items.ItemName)
+            cmd.Parameters.AddWithValue("@ItemCode", Items.ItemCode)
+            cmd.Parameters.AddWithValue("@UnitTypeID", Items.UnitTypeID)
+            cmd.Parameters.AddWithValue("@LowBalanceReport", Items.LowBalanceReport)
+            cmd.Parameters.AddWithValue("@MaxAllowableRequisition", Items.MaxAllowableRequisition)
+            cmd.Parameters.AddWithValue("@IsActive", Items.IsActive)
+            cmd.Parameters.AddWithValue("@EntryBy", Items.EntryBy)
+            cmd.ExecuteNonQuery()
+            con.Close()
+            Return 1
+        Catch ex As Exception
+            If con.State = ConnectionState.Open Then
+                con.Close()
+            End If
+            Return 0
+        End Try
+    End Function
+
+#End Region
+
+#Region " Get Item List "
+
+    Public Function fnGetItemList() As DataSet
+
+        Dim sp As String = "spGetItemList"
+        Dim da As SqlDataAdapter = New SqlDataAdapter()
+        Dim ds As DataSet = New DataSet()
+        Try
+            con.Open()
+            Using cmd = New SqlCommand(sp, con)
+                cmd.CommandType = CommandType.StoredProcedure
+                da.SelectCommand = cmd
+                da.Fill(ds)
+                con.Close()
+                Return ds
+            End Using
+        Catch ex As Exception
+            If con.State = ConnectionState.Open Then
+                con.Close()
+            End If
+            Return Nothing
+        End Try
+    End Function
+
+#End Region
+
+#Region " Get Requisition Remmaining Item List "
+
+    Public Function fnGetReqRemItemList() As DataSet
+
+        Dim sp As String = "spGetReqRemItemList"
+        Dim da As SqlDataAdapter = New SqlDataAdapter()
+        Dim ds As DataSet = New DataSet()
+        Try
+            con.Open()
+            Using cmd = New SqlCommand(sp, con)
+                cmd.CommandType = CommandType.StoredProcedure
+                da.SelectCommand = cmd
+                da.Fill(ds)
+                con.Close()
+                Return ds
+            End Using
+        Catch ex As Exception
+            If con.State = ConnectionState.Open Then
+                con.Close()
+            End If
+            Return Nothing
+        End Try
+    End Function
+
+#End Region
+
+#Region " Get Item List Details "
+
+    Public Function fnGetItemListDetails() As DataSet
+
+        Dim sp As String = "spGetItemListDetails"
+        Dim da As SqlDataAdapter = New SqlDataAdapter()
+        Dim ds As DataSet = New DataSet()
+        Try
+            con.Open()
+            Using cmd = New SqlCommand(sp, con)
+                cmd.CommandType = CommandType.StoredProcedure
+                da.SelectCommand = cmd
+                da.Fill(ds)
+                con.Close()
+                Return ds
+            End Using
+        Catch ex As Exception
+            If con.State = ConnectionState.Open Then
+                con.Close()
+            End If
+            Return Nothing
+        End Try
+    End Function
+
+#End Region
+
+#Region " Get Low Balance Item List "
+
+    Public Function fnGetLowBalanceItemList() As DataSet
+
+        Dim sp As String = "spGetLowBalanceItemList"
+        Dim da As SqlDataAdapter = New SqlDataAdapter()
+        Dim ds As DataSet = New DataSet()
+        Try
+            con.Open()
+            Using cmd = New SqlCommand(sp, con)
+                cmd.CommandType = CommandType.StoredProcedure
+                da.SelectCommand = cmd
+                da.Fill(ds)
+                con.Close()
+                Return ds
+            End Using
+        Catch ex As Exception
+            If con.State = ConnectionState.Open Then
+                con.Close()
+            End If
+            Return Nothing
+        End Try
+    End Function
+
+#End Region
+
+End Class
